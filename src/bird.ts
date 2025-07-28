357a7195152345a9146effaee2b47a6032b9b472
krasi-veleva <krasiveleva2002@gmail.com>
2025-07-28 08:51:30 +0300

    feat: initialize sprite position & anchor and add keyboard-based movement

--- src/bird.ts
+++ src/bird.ts
@@ -1,5 +1,5 @@
 import * as PIXI from "pixi.js";
-import { setInterval } from "timers";
+import { gameWidth, gameHeight } from "./gameSettings";
 
 export class Bird extends PIXI.AnimatedSprite {
     constructor() {
@@ -12,27 +12,30 @@ export class Bird extends PIXI.AnimatedSprite {
         this.animationSpeed = 0.1;
         this.play();
         this.scale.set(3);
+        this.anchor.set(0.5, 0.5);
+        this.position.set(gameWidth / 2, gameHeight / 2);
+
+        window.addEventListener("keydown", (e) => {
+            this.moveLeftAndRight(e);
+        });
     }
 
-    moveLeft(): void {
-        setInterval(() => {
-            console.log("bird move left", this.x);
-            this.x -= 1;
-        }, 1000);
+    moveLeftAndRight(event: KeyboardEvent): void {
+        const key = event.key;
+        const bound = this.width / 2;
+
+        if (key === "ArrowLeft") {
+            if (this.x <= 0 + bound) {
+                return;
+            }
+            this.x -= 10;
+            console.log("left key pressed");
+        } else if (key === "ArrowRight") {
+            if (this.x >= gameWidth - bound) {
+                return;
+            }
+            this.x += 10;
+            console.log("right key pressed");
+        }
     }
 }
-
-// function getBird(): PIXI.AnimatedSprite {
-//     const animSprite = new PIXI.AnimatedSprite([
-//         PIXI.Texture.from("birdUp.png"),
-//         PIXI.Texture.from("birdMiddle.png"),
-//         PIXI.Texture.from("birdDown.png"),
-//     ]);
-
-//     animSprite.loop = true;
-//     animSprite.animationSpeed = 0.1;
-//     animSprite.play();
-//     animSprite.scale.set(3);
-
-//     return animSprite;
-// }
