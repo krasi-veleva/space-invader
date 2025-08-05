5a2767a6823c12fbcd5c5da284f3e39d0f824501
krasi-veleva <krasiveleva2002@gmail.com>
2025-08-05 08:44:17 +0300

    feat: position bullet at center of canvas

--- src/bullet.ts
+++ src/bullet.ts
@@ -1,11 +1,14 @@
 import * as PIXI from "pixi.js";
+import { gameHeight, gameWidth } from "./gameSettings";
 
 export class Bullet extends PIXI.Graphics {
     constructor() {
         super();
 
         this.beginFill(0xde3249);
-        this.drawRect(0, 0, 100, 100);
+        this.drawRect(0, 0, 5, 20);
         this.endFill();
+
+        this.position.set(gameWidth / 2, gameHeight / 2);
     }
 }
