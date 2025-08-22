7daaa0a743b7f86b6ce82d459bd9b28b21a4b980
krasi-veleva <krasiveleva2002@gmail.com>
2025-08-22 08:12:00 +0300

    feat: update ship movement

--- src/ship.ts
+++ src/ship.ts
@@ -1,50 +1,67 @@
-import { AnimatedSprite, Texture } from "pixi.js";
-import gsap from "gsap";
+import { Sprite, Texture, Ticker } from "pixi.js";
 import { gameHeight, gameWidth } from "./gameSettings";
 
-export class Ship extends AnimatedSprite {
-    constructor() {
-        super([Texture.from("ship")]);
+export class Ship extends Sprite {
+    private keys = new Set<string>();
+    private ticker = Ticker.shared;
 
-        this.loop = true;
-        this.animationSpeed = 0.1;
-        this.play();
+    constructor() {
+        super(Texture.from("ship"));
+
+        this.init();
+
+        window.addEventListener("keydown", (e) => {
+            this.onKeyDown(e);
+        });
+
+        window.addEventListener("keyup", (e) => {
+            this.onKeyUp(e);
+        });
+
+        this.ticker.add(this.moveRightOrLeft.bind(this));
+    }
+
+    private init() {
         this.scale.set(0.2);
         this.anchor.set(0.5, 0.5);
         this.position.set(gameWidth / 2, gameHeight - this.height / 2);
-
-        window.addEventListener("keydown", (e) => {
-            this.moveLeftAndRight(e);
-        });
     }
 
-    public moveLeftAndRight(event: KeyboardEvent): void {
+    private onKeyDown(event: KeyboardEvent) {
         const key = event.key;
-        const bound = this.width / 2;
-        const shipX: number = this.x;
 
-        if (key === "ArrowLeft") {
+        if (key === "ArrowLeft" || key === "ArrowRight") {
+            this.keys.add(key);
+        }
+
+        console.log(this.keys);
+    }
+
+    private onKeyUp(event: KeyboardEvent) {
+        const key = event.key;
+
+        if (key === "ArrowLeft" || key === "ArrowRight") {
+            this.keys.delete(key);
+        }
+    }
+
+    private moveRightOrLeft() {
+        const bound = this.width / 2;
+
+        if (this.keys.has("ArrowLeft")) {
             if (this.x <= 0 + bound) {
                 return;
             }
 
-            gsap.to(this, {
-                x: shipX - 45,
-                duration: 0.1,
-                ease: "none",
-            });
-            console.log("left key pressed");
-        } else if (key === "ArrowRight") {
+            this.x -= 10;
+        }
+
+        if (this.keys.has("ArrowRight")) {
             if (this.x >= gameWidth - bound) {
                 return;
             }
 
-            gsap.to(this, {
-                x: shipX + 45,
-                duration: 0.1,
-                ease: "none",
-            });
-            console.log("right key pressed");
+            this.x += 10;
         }
     }
 }
