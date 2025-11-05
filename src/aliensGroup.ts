a61b4da4b9b70d55bd9deee33e2b46d7d1019b09
krasi-veleva <krasiveleva2002@gmail.com>
2025-11-05 08:04:15 +0200

    feat: make aliens movement

--- src/aliensGroup.ts
+++ src/aliensGroup.ts
@@ -1,13 +1,19 @@
-import { Container } from "pixi.js";
+import { Container, Ticker } from "pixi.js";
 import { Alien } from "./alien";
+import { gameWidth } from "./gameSettings";
 
 export class AliensGroup extends Container {
     private aliens: Alien[] = [];
     static readonly Y_SPACING = 61;
 
+    private speed = 2;
+    private direction: 1 | -1 = 1;
+    private ticker: Ticker = Ticker.shared;
+
     constructor() {
         super();
         this.renderAliens();
+        this.ticker.add(this.aliensMovement.bind(this));
     }
 
     private renderAliens(): void {
@@ -21,5 +27,19 @@ export class AliensGroup extends Container {
             this.aliens.push(alien);
             this.addChild(alien);
         }
+    }
+
+    private aliensMovement(): void {
+        if (this.x >= gameWidth - this.width) {
+            this.direction = -1;
+            this.y += 20;
+        }
+
+        if (this.x <= 0) {
+            this.direction = 1;
+            this.y += 20;
+        }
+
+        this.x += this.speed * this.direction * this.ticker.deltaTime;
     }
 }
