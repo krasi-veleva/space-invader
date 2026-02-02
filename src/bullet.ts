495bcb41fac194a293be195836961dbdbba11006
krasi-veleva <krasiveleva2002@gmail.com>
2026-02-02 08:08:53 +0200

    feat: implement bullet movement with space button

--- src/bullet.ts
+++ src/bullet.ts
@@ -1,13 +1,45 @@
-import { Sprite, Texture } from "pixi.js";
-import { gameHeight, gameWidth } from "./gameSettings";
+import { Sprite, Texture, Ticker } from "pixi.js";
+import { Game } from "./game";
 
 export class Bullet extends Sprite {
-    constructor() {
+    private ticker = Ticker.shared;
+    private onTick: () => void;
+    private speed = 10;
+
+    constructor(private game: Game) {
         super(Texture.from("bullet"));
 
+        this.init();
+
+        this.onTick = this.bulletMovement.bind(this);
+
+        this.start();
+    }
+
+    private init() {
         this.scale.set(0.1);
         this.anchor.set(0.5, 0.5);
+    }
 
-        this.position.set(gameWidth / 2, gameHeight / 2);
+    private bulletMovement() {
+        const gameBounder = this.height - 15;
+
+        this.y -= this.speed;
+
+        if (this.game.bullet && this.y <= gameBounder) {
+            console.log("destroying because out of screen");
+            this.destroyBullet();
+        }
+    }
+
+    private start() {
+        this.ticker.add(this.onTick);
+    }
+
+    private destroyBullet() {
+        this.ticker.remove(this.onTick);
+
+        this.destroy();
+        console.log("bullet is destroyed");
     }
 }
--- src/game.ts
+++ src/game.ts
@@ -4,16 +4,35 @@ import { Bullet } from "./bullet";
 import { AliensGroup } from "./aliensGroup";
 
 export class Game extends Container {
+    private keys = new Set<string>();
+    public ship: Ship;
+    public bullet?: Bullet;
+
     constructor(stage: Container) {
         super();
-        const shipFromSprite = new Ship(stage);
 
-        const bullet = new Bullet();
-
+        this.ship = new Ship(stage);
         const aliens = new AliensGroup();
 
-        stage.addChild(shipFromSprite);
-        stage.addChild(bullet);
+        stage.addChild(this.ship);
         stage.addChild(aliens);
+
+        window.addEventListener("keydown", (e) => {
+            this.onSpacePressed(e, stage);
+        });
+    }
+
+    private onSpacePressed(event: KeyboardEvent, stage: Container) {
+        if (event.key === " " || event.code === "Space") {
+            this.keys.add(event.code);
+            console.log("space pressed");
+            console.log(this.keys);
+
+            this.bullet = new Bullet(this);
+
+            this.bullet.position.set(this.ship.x, this.ship.y - 65);
+
+            stage.addChild(this.bullet);
+        }
     }
 }
